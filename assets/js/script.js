f57a6da0d9d33352d9aa3727a63d94bc61e4661b
Alys Scorpio <alyssad1182@gmail.com>
2021-08-02 17:07:42 -0700

    Laying out functions in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,16 @@ var today = moment().format("dddd, MMMM Do, YYYY");
 
 // Variables from HTML elements
 var forcast = document.querySelector('#weather-forcast');
-var currentWeather = document.querySelector('#weather-forcast');
-var fiveDay = document.querySelector("#five-day");
+var currentWeather = document.querySelector('#current-weather');
+var fiveDay = document.querySelector("#five-day-forcast");
 var searchBtn = document.querySelector('#search-btn');
+
+function displayWeatherNow() {
+
+};
+
+function displayWeatherLater() {
+
+};
+
+searchBtn.addEventListener("click", displayWeatherNow);

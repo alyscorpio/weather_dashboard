8efcf8abd73878faab11164107145256521f8f23
Alys Scorpio <alyssad1182@gmail.com>
2021-08-02 17:04:19 -0700

    Creating variables from html elements

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// Current date from moment js
+var today = moment().format("dddd, MMMM Do, YYYY");
+
+// Variables from HTML elements
+var forcast = document.querySelector('#weather-forcast');
+var currentWeather = document.querySelector('#weather-forcast');
+var fiveDay = document.querySelector("#five-day");
+var searchBtn = document.querySelector('#search-btn');
